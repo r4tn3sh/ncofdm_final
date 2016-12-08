7fc430d7bf6e92105a7d22ad2a24299113aff385
ratnesh <ratnesh.kumbhkar@gmail.com>
2016-12-08 11:40:27 -0500

    Added noise but doesn't work properly

--- src/underlay.cpp
+++ src/underlay.cpp
@@ -1,8 +1,12 @@
 #include "underlay.h"
 #include <numeric>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
 
 #define TOTAL_SIZE 240
+#define UL_AMP 0.0159
 namespace wno
 {
     underlay::underlay()
@@ -12,6 +16,7 @@ namespace wno
     {
         std::vector<double>abs_data(overlay_data.size());// = std::abs(overlay_data);
         std::cout << "Buffer of size "<< overlay_data.size() << std::endl;
+        std::srand(std::time(0)); // use current time as seed for random generator
         // for(int x = 0; x < overlay_data.size(); x++)
         // {
         //     abs_data[x] = std::abs(overlay_data[x]);
@@ -22,6 +27,9 @@ namespace wno
         // Assuming that average amplitude of overlay_data is ~0.1
         int z = 0;
         int polarity = 1; // transmit alternate +1 and -1
+        double noise;
+        std::default_random_engine generator;
+        std::normal_distribution<double> distribution(0.1*UL_AMP,0.1*UL_AMP);
         std::vector<std::complex<double> > output = overlay_data;
         for(int x = 0; x < overlay_data.size(); x++)
         {
@@ -29,9 +37,11 @@ namespace wno
             // Add 240 240 80 80 80 ......
             // output[x] += std::complex<double>(0.00316227766,0)*SPNS[z]; // adding signal 15dB below
             // output[x] += std::complex<double>(0.1*abs_data[x],0)*SPNS[z]; // adding signal 15dB below
-            //
             // output[x] += std::complex<double>(polarity*0.019342,0)*SPNS[z]; // adding signal 12dB below
-            output[x] += std::complex<double>(polarity*0.0159,0)*SPNS[z]; // adding signal 12dB below
+            output[x] += std::complex<double>(polarity*UL_AMP,0)*SPNS[z]; // adding signal 12dB below
+            noise = distribution(generator);
+            std::cout << noise << std::endl;
+            output[x] += std::complex<double>(noise,0); // adding signal 12dB below
             z++;
             if(z>pnSize-1)
             {
