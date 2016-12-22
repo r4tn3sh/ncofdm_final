c356b4b232bdb18e2f5a3df0da252e962fa77991
ratnesh <ratnesh.kumbhkar@gmail.com>
2016-12-22 15:22:19 -0500

    Created a simple transmitter, test not done yet

--- README.md
+++ README.md
@@ -12,5 +12,7 @@ cmake ../
 make
 ### Running code
 Once you run the *make*, there should be a *bin* subfolder within *ncofdm_final* folder. This subfolder contains executable binaries. Currently it contains following examples
-##### ./nc_sim
+#### ./nc_sim
 Runs a simulation where the transmitter sends NC-OFDM symbols over a given set of subcarriers. An underlay channel has been added to help with timing, which helps in identfying the start of the frame.
+#### ./test_nc_tx
+Basic NC-OFDM transmitter. Currently has not been tested completely. 
---
+++ src/nc_transmitter.cpp
@@ -0,0 +1,39 @@
+/*! \file transmitter.cpp
+ *  \brief C++ file for the transmitter class.
+ *
+ *  The transmitter class is the public interface for the wno_ofdm transmit chain.
+ */
+
+#include "nc_transmitter.h"
+
+namespace wno {
+
+    /*!
+     *  This constructor shows exactly what parameters need to be set for the transmitter
+     */
+    nc_transmitter::nc_transmitter(uint64_t sc_map, double freq, double samp_rate, double tx_gain, double tx_amp, std::string device_addr) :
+        m_sc_map(sc_map),
+        m_usrp(usrp_params(freq, samp_rate, tx_gain, 20, tx_amp, device_addr)),
+        m_nc_frame_builder()
+    {
+    }
+
+    /*!
+     * This construct is for those who feel more comfortable using the usrp_params struct
+     */
+    nc_transmitter::nc_transmitter(usrp_params params) :
+        m_usrp(params),
+        m_nc_frame_builder()
+    {
+    }
+
+    /*!
+     *  Transmits a single frame, blocking until the frame is sent.
+     */
+    void nc_transmitter::send_frame(std::vector<unsigned char> payload, Rate phy_rate)
+    {
+        std::vector<std::complex<double> > samples = m_nc_frame_builder.build_frame(payload, phy_rate, m_sc_map);
+        m_usrp.send_burst_sync(samples);
+    }
+
+}
